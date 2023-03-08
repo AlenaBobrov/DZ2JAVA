64bad8427a5310cd6b4b68545b884eeebd384948
Alena Bobrova <kaisa8721@gmail.com>
2023-03-08 17:59:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketprice = 1453;
+        int onemile = 50;
+
+        System.out.println(ticketprice / onemile);
+    }
+}
