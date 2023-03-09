5450fb160327fe09560a004181e8fa6d13081472
Alena Bobrova <125354102+AlenaBobrov@users.noreply.github.com>
2023-03-09 08:08:38 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketprice = 1453;
-        int onemile = 20;
+        int ticketPrice = 1453;
+        int onemMile = 20;
 
-        System.out.println(ticketprice / onemile);
+        System.out.println(ticketPrice / onemMile);
     }
 }
